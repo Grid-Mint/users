fc435bd95dc6d853ce34f07aa801f738e262edb2
Denissius Maximus <deniskramar07@gmail.com>
2026-07-31 21:02:27 +0300

    fix: correct response message in Handler function

--- src/api/handler.go
+++ src/api/handler.go
@@ -8,5 +8,5 @@ import (
 func Handler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%s %s", r.Method, r.URL.Path)
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("ти, підор!"))
+	w.Write([]byte("ти підор"))
 }
