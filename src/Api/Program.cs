a1cc40b56a93471822205adc7b452f6be60c65d1
Denissius Maximus <deniskramar07@gmail.com>
2026-08-18 21:52:59 +0300

    feat: set up Serilog for logging and add global exception handler

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,7 +1,26 @@
+using Serilog;
+using Users.Api.Utils;
+using Users.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog(((context, configuration) => configuration
+        .WriteTo.Console()
+        .WriteTo.File("logs/log-.log", rollingInterval: RollingInterval.Day)
+        .ReadFrom.Configuration(context.Configuration)
+    ));
+
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
 var app = builder.Build();
+
+app.UseExceptionHandler();
+app.UseSerilogRequestLogging();
 
 app.MapGet("/users", () => "[users]");
 app.MapGet("/user/{id}", (int id) => $"user {id}");
+
 
 app.Run();
